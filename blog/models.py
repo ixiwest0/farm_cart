0fa1b5aca36f16095b05f64b1a99c1ba35947553
jihyun923 <152951869+jihyun923@users.noreply.github.com>
2024-12-22 19:51:12 +0900

    Update models.py
    바꿈

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import os
-from markdownx.models import MarkdownxField
-
 
 class Tag(models.Model):
     name = models.CharField(max_length=50)
@@ -31,22 +29,25 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
-
-    head_image = models.ImageField(upload_to="blog/images/&Y/%m/%d/", blank=True)
-    # blank=Ture로 지정하지 않으면 title필드를 빈칸으로 저장할 경우 경고 메시지 출력됨
+    price = models.DecimalField(max_digits=10, decimal_places=0, default=0)
+    stock = models.PositiveIntegerField(default=0)
+    head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
     file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d/", blank=True)
+    available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    category = models.ForeignKey(
-        Category, null=True, blank=True, on_delete=models.SET_NULL
-    )
+    category = models.ManyToManyField(Category, blank=True)
 
     tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return f"[{self.pk}]{self.title} :: {self.author}"
